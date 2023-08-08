d2f626463aef37e9b4ecd9c9bd9b49037594c6fa
“KKasodekar” <kunalkasodekar@rocketcentral.com>
2023-08-08 14:09:27 -0400

    Feature Update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,10 +7,11 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
   heroes = HEROES
-  hero: Hero ={
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero? : Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
